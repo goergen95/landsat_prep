4662fd627254eeeb465eac1990f8099fa67d2e74
goergen95 <44396184+goergen95@users.noreply.github.com>
2018-11-17 17:19:55 +0100

    Update get_Landsat_Info.R

--- src/get_Landsat_Info.R
+++ src/get_Landsat_Info.R
@@ -1,12 +1,18 @@
 ### Pseudo Code Water Quality IBB Script
 ## Set Up Environment
 source("~/lsat/landsat_prep/src/env_setup.R")
-
+library(mapview)
 
 ## read in shapefile of study area
 #shape <- readOGR(dsn = paste0(envrmt$shapes,"name_of_shapefile.shp"))
 ## or enter bounding box manually
-aoi <- matrix(data=c(47.26,73.13,47.26,83.11,43.38,83.11,43.38,73.13),nrow=4,byrow=TRUE)
+aoi <- matrix(data=c(83.11,47.26,73.13,47.26,73.13,43.38,83.11,43.38),nrow=4,byrow=TRUE)
+
+#y <- spatial.tools::bbox_to_SpatialPolygons(bbox(aoi))
+#crs(y) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+#mapview(y)
+
+
 #you may want to set the aoi session wide with
 #set_aoi(aoi)
 
@@ -29,7 +35,7 @@ fileListOT <- fileList[which(fileList$SensorIdentifier=="OLI_TIRS"),]
 
 #you might use this call to download data within R
 #save order ID here and specify it afer abortion with: espa_order = ""
-getLandsat_data(fileListOT, level ="l1", source = "AWS", dir_out = envrmt$path_data_raw,
+getLandsat_data(fileListOT, level ="sr", source = "ESPA", dir_out = envrmt$path_data_raw,
                 username = "da.goergen",force =TRUE)
 
 
@@ -37,5 +43,5 @@ getLandsat_data(fileListOT, level ="l1", source = "AWS", dir_out = envrmt$path_d
 #if it doesnt work, write out the IDs of scenes of interest and do a file ordering at USGS website
 #and use the bulk download application there
 
-write.csv(as.vector(fileList$displayId), file = paste0(envrmt$path_data,"list.csv"),quote = FALSE, sep =",",row.names = FALSE,col.names = FALSE)
+#write.csv(as.vector(fileList$displayId), file = paste0(envrmt$path_data,"list.csv"),quote = FALSE, sep =",",row.names = FALSE,col.names = FALSE)
 
